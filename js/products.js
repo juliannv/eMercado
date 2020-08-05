a3998c73a66c0309cb39957ab9ba1c0aa4abd8b8
juliannv <azargut@gmail.com>
2020-08-05 18:11:29 -0300

    Commit 2 - Login y Productos

--- js/products.js
+++ js/products.js
@@ -1,6 +1,41 @@
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += ` 
+        <div>
+            <h2>` + category.name + `</h2>
+        </div>
+        <div>
+            <img src="` + category.imgSrc + `">
+        </div>
+        <div>
+            <p>` + category.description + `</p>
+            <p>Precio: ` + category.cost + ` ` + category.currency + `</p>
+            <p>Vendidos: ` + category.soldCount + `</p>
+        </div>
+        `
+
+        document.getElementById("listado").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
