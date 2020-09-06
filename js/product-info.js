a12cb96bd865b82d8888366bd7474e3f58ab6969
juliannv <azargut@gmail.com>
2020-09-06 15:51:48 -0300

    Commit 4.0 - Comienzo entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -1,3 +1,73 @@
-document.addEventListener("DOMContentLoaded", function(e){
+var product = {};
+var commentList = [];
 
+function showImagesGallery(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+    }
+}
+
+function showCommentList(array){
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let comment = array[i];
+           
+        htmlContentToAppend += `
+            <hr>
+            <br><br>
+            <p>Puntuación: ` + comment.score + `<p>
+            <br>
+            <p> ` + comment.description + `<p>
+            <br>
+            <p>Usuario: ` + comment.user + `<p>
+            <br>
+            <p>Fecha: ` + comment.dateTime + `<p>
+            <br><br>
+        `
+
+        document.getElementById("comment-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCostHTML = document.getElementById("productCost");
+            let productSoldCountHTML = document.getElementById("productSoldCount");
+           
+        
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML = product.cost + " " + product.currency;
+            productSoldCountHTML.innerHTML = product.soldCount;
+
+            showImagesGallery(product.images);
+        }
+    });
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            showCommentList(resultObj.data);
+        }
+    });
 });
--- js/products.js
+++ js/products.js
@@ -48,19 +48,21 @@ function showProductList() {
             (valorBuscador == "" || (nombreMin.includes(valorBuscador.toLowerCase()) || descripcionMin.includes(valorBuscador.toLowerCase())))) {
 
             htmlContentToAppend += `
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ product.name + `</h4>
-                        <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                     </div>
-                    <p class="mb-1">` + product.description + `</p> <br>
-                    <p class="mb1-1">Precio: ` + product.cost + ` ` + product.currency + `<p>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name + `</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p> <br>
+                        <p class="mb1-1">Precio: ` + product.cost + ` ` + product.currency + `<p>
+                    </div>
                 </div>
-            </div>
+            </a>
         `
         }
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
